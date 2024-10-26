374119d9b9c131f9ce1d83b6070203cf81d13e49
Victor Chepur <94067324+vchepur@users.noreply.github.com>
2024-10-26 11:48:57 +0200

    4
    4

--- src/app/boats/gelex/g5-2/page.js
+++ src/app/boats/gelex/g5-2/page.js
@@ -3,18 +3,16 @@ import ImageGalleryComponent from '/src/app/components/ImageGalleryComponent'
 
 export default function ExamplePage() {
     const images = [
-        { src: '/boats/gelex/G4-5/1.jpg', alt: 'Boat 1' },
-        { src: '/boats/gelex/G4-5/2.jpg', alt: 'Boat 2' },
-        { src: '/boats/gelex/G4-5/3.jpg', alt: 'Boat 3' },
-        { src: '/boats/gelex/G4-5/4.jpg', alt: 'Boat 4' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
-        { src: '/boats/gelex/G4-5/5.jpg', alt: 'Boat 5' },
+        { src: '/boats/gelex/g4-5/1.jpg', alt: 'Boat 1' },
+        { src: '/boats/gelex/g4-5/2.jpg', alt: 'Boat 2' },
+        { src: '/boats/gelex/g4-5/3.jpg', alt: 'Boat 3' },
+        { src: '/boats/gelex/g4-5/4.jpg', alt: 'Boat 4' },
+        { src: '/boats/gelex/g4-5/5.jpg', alt: 'Boat 5' },
+        { src: '/boats/gelex/g4-5/1.jpg', alt: 'Boat 1' },
+        { src: '/boats/gelex/g4-5/2.jpg', alt: 'Boat 2' },
+        { src: '/boats/gelex/g4-5/3.jpg', alt: 'Boat 3' },
+        { src: '/boats/gelex/g4-5/4.jpg', alt: 'Boat 4' },
+        { src: '/boats/gelex/g4-5/5.jpg', alt: 'Boat 5' },
 
     ];
 
