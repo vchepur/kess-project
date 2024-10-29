6905cfd6062c705655aa34accd3a504d397325cf
Victor Chepur <94067324+vchepur@users.noreply.github.com>
2024-10-29 19:08:37 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import i18n from './next-i18next.config.js';
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    ...i18n,
+};
 
 export default nextConfig;
